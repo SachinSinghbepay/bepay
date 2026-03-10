a3c58765091e48e075943e8cc0abe3fa51ef0648
SachinSinghbepay <sachin@bepay.money>
2026-03-10 10:36:37 +0530

    minor image fix

--- components/igps/compliance.jsx
+++ components/igps/compliance.jsx
@@ -34,7 +34,7 @@ const complianceData = [
       { name: "RBI's PA-CB*" },
       { name: "FEMA" },
       { name: "DORA" },
-      { name: "DPDP", logo: "/icons/dpdp.svg" },
+      { name: "DPDP", logo: "/icons/DPDP.svg" },
       { name: "MiCA-ready", logo: "/icons/MiCA-ready.svg" },
       { name: "CFT" },
     ],
